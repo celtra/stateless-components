6cc7485bd67127a7c11cffe567c1e9da9aec0c2e
zan <zan.kusterle@gmail.com>
2018-06-20 16:01:58 +0200

    use named exports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import components from './components'
+import CheckboxComponent from '@/stateless/checkbox.vue'
+import DialogButtonComponent from '@/stateless/dialog_button.vue'
+import DialogComponent from '@/stateless/dialog.vue'
+import DropAreaComponent from '@/stateless/drop_area.vue'
+import FileUploadRequirementsComponent from '@/stateless/file_upload_requirements.vue'
+import FileUploadComponent from '@/stateless/file_upload.vue'
+import ImageListComponent from '@/stateless/image_list.vue'
+import InputComponent from '@/stateless/input.vue'
+import MultiselectComponent from '@/stateless/multiselect.vue'
+import RadioButtonComponent from '@/stateless/radiobutton.vue'
+import SelectboxComponent from '@/stateless/selectbox.vue'
+import SupportTextComponent from '@/stateless/support_text.vue'
+import IconComponent from '@/stateless/icon.vue'
 
-var data = {}
-Object.keys(components).forEach(componentId => {
-    data[componentId] = components[componentId].component
-})
-
-export default data
+export const Checkbox = CheckboxComponent
+export const DialogButton = DialogButtonComponent
+export const Dialog = DialogComponent
+export const DropArea = DropAreaComponent
+export const FileUploadRequirements = FileUploadRequirementsComponent
+export const FileUpload = FileUploadComponent
+export const ImageList = ImageListComponent
+export const Input = InputComponent
+export const Multiselect = MultiselectComponent
+export const RadioButton = RadioButtonComponent
+export const Selectbox = SelectboxComponent
+export const SupportText = SupportTextComponent
+export const Icon = IconComponent
