a7df8786362f52b018875902775cb07cf1a51a26
zigomir <zigomir@gmail.com>
2018-06-21 10:22:44 +0200

    Add multiselect default props so we can use it.

--- src/components.js
+++ src/components.js
@@ -53,6 +53,22 @@ export default {
     },
     Multiselect: {
         component: Multiselect,
+        defaultProps: {
+            options: [
+                { id: '1', label: "Something", metadata: 'zan.kusterle@gmail.com', icon: 'plus' },
+                { id: '2', label: "Lorem" },
+                { id: '3', label: "Ipsum", metadata: 'someone@lorem.ipsum' },
+                { id: '4', label: "A", metadata: 'someone@lorem.ipsum' },
+                { id: '5', label: "B", metadata: 'someone@lorem.ipsum' },
+                { id: '6', label: "C", metadata: 'someone@lorem.ipsum' },
+                { id: '7', label: "D", metadata: 'someone@lorem.ipsum' },
+                { id: '8', label: "E", metadata: 'someone@lorem.ipsum' },
+                { id: '9', label: "F", metadata: 'someone@lorem.ipsum' },
+                { id: '10', label: "G", metadata: 'someone@lorem.ipsum' },
+            ],
+            value: [],
+            autoReorder: false,
+        },
     },
     RadioButton: {
         component: RadioButton,
