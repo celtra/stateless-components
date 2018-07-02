d7a8b8ab26b05f296896cd550dd5f67a353414f0
Matevz Stinjek <matevz.stinjek@celtra.com>
2018-07-02 20:03:43 +0200

    Added group component

---
+++ src/stateless/group.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="group">
+        <div class="group__title">
+            <div>{{ label }}</div>
+            <support-text
+                v-if="description"
+                class="group__info"
+                :text="description"
+            ></support-text>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import SupportText from './support_text.vue'
+
+export default {
+    components: {
+        supportText: SupportText
+    },
+    props: {
+        label: { type: String },
+        description: { type: String, required: false }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import (reference) "variables";
+
+.group {
+  border: 1px solid @gunpowder;
+  padding: 30px;
+  margin-top: 10px;
+  position: relative;
+
+  &__title {
+    color: @dolphin;
+    font-size: 18px;
+    position: absolute;
+    top: -11px;
+    background-color: @extremely-dark-gray;
+    padding: 0 10px;
+    display: flex;
+  }
+
+  &__info {
+    width: 16px;
+    height: 16px;
+    padding: 3px 6px 0 16px;
+  }
+}
+</style>
