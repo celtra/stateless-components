cfd13a13431132ee1a88b2c40fcd376a681407e7
Jakob Košir <kosir.jakob@gmail.com>
2018-08-17 13:14:23 +0200

    Revert "Extract multiline list item styles & fix hover colors"
    This reverts commit 6b23d9d87e48d56ea31a0c2c38cb1a6676b5c385.

--- src/stateless/MultilineListItem.vue
+++ src/stateless/MultilineListItem.vue
@@ -84,8 +84,119 @@ export default {
 </script>
 
 <style lang="less" scoped>
-/*
-  Multiline list item styles are in multiline_list_item.less.
-  They are imported in parent component so they can be overridden (colors for hover states etc.)
-*/
+@import (reference) './variables';
+
+.multiline-list-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+
+    &--disabled {
+        cursor: auto;
+    }
+
+    &__label {
+        margin: 0;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-family: @regular-text-font;
+        color: @very-light-gray;
+        display: inline-block;
+
+        &--error { color: @pink-red; }
+        &--disabled { color: @gray-blue; }
+        &--with-metadata { padding-right: 5px; }
+    }
+
+    &__metadata {
+        display: flex;
+        align-items: center;
+        margin: 0;
+        white-space: nowrap;
+        text-align: right;
+        overflow: hidden;
+        font-family: @regular-text-font;
+        color: @gray-blue;
+    }
+
+    &__icon {
+        margin-left: 10px;
+    }
+
+    &__hidden-widths {
+        visibility: hidden;
+        position: absolute;
+    }
+}
+
+.multiline-list-item--dark {
+    .multiline-list-item {
+        &__label {
+            color: @very-light-gray;
+            &--selected { color: @black; }
+            &:hover {
+                &:not(.multiline-list-item--disabled)&:not(.multiline-list-item--selected) {
+                    .multiline-list-item__label { color: @white; }
+                }
+            }
+        }
+    }
+}
+
+.multiline-list-item--light {
+    .multiline-list-item {
+        &__label {
+            color: @gunpowder;
+            &--selected { color: @royal-blue; }
+            &:hover {
+                &:not(.multiline-list-item--disabled)&:not(.multiline-list-item--selected) {
+                    .multiline-list-item__label { color: @black; }
+                }
+            }
+        }
+    }
+}
+
+.multiline-list-item--condensed {
+    height: 20px;
+
+    .multiline-list-item {
+        &__label {
+            font-size: 14px;
+        }
+
+        &__metadata {
+            font-size: 11px;
+            letter-spacing: 0.5px;
+        }
+    }
+}
+
+.multiline-list-item--normal {
+    .multiline-list-item {
+        &__label {
+            font-size: 18px;
+        }
+
+        &__metadata {
+            font-size: 12px;
+        }
+    }
+}
+
+.multiline-list-item--phat {
+    height: 45px;
+
+    .multiline-list-item {
+        &__label {
+            font-size: 22px;
+        }
+
+        &__metadata {
+            font-size: 14px;
+        }
+    }
+}
 </style>
