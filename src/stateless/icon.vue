83c99a817e6412f0d7f4a7c49e7190896c5fa058
Jakob Košir <kosir.jakob@gmail.com>
2018-09-10 15:10:35 +0200

    Remove leftover

--- src/stateless/icon.vue
+++ src/stateless/icon.vue
@@ -99,7 +99,7 @@
         <!-- 'clear': X sign, transparent and encircled -->
         <svg v-if="name === 'clear'" :style="styleOverride" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
             <path class="path" d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm3.5 10.1l-1.4 1.4L8 9.4l-2.1 2.1-1.4-1.4L6.6 8 4.5 5.9l1.4-1.4L8 6.6l2.1-2.1 1.4 1.4L9.4 8l2.1 2.1z" fill-rule="nonzero"/>
-        </svg>se
+        </svg>
 
         <!-- 'clear-small': X sign, bold -->
         <svg v-if="name === 'x-bold'" :style="styleOverride" viewBox="0 0 8 8" xmlns="http://www.w3.org/2000/svg">
