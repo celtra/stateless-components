95be98984062228ffc2b4c700065458bad429191
zan <zan.kusterle@gmail.com>
2018-09-14 15:37:06 +0200

    chip remove button hitbox

--- src/stateless/Chip.vue
+++ src/stateless/Chip.vue
@@ -2,8 +2,8 @@
     <div :class="[theme, { active: isActive, disabled: isDisabled }] | prefix('chip--')" class="chip" tabindex="0" @click="$emit('click')">
         <div class="chip__label">{{ label }}</div>
         <div v-if="metadata" class="chip__metadata">{{ metadata }}</div>
-        <span v-if="isRemovable" @click.stop="$emit('remove')">
-            <icon style="width: 8px; height: 8px" class="chip__remove-btn" name="x-bold"></icon>
+        <span v-if="isRemovable" class="chip__remove-btn" @click.stop="$emit('remove')">
+            <icon style="width: 8px; height: 8px" name="x-bold"></icon>
         </span>
     </div>
 </template>
@@ -58,7 +58,8 @@ export default {
 
 .chip__remove-btn {
     transition: all @default-transition-time;
-    padding: 0 0 0 5px;
+    padding: 0 5px;
+    margin-right: -5px;
 }
 
 /* DARK THEME */
