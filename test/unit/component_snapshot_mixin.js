a192c95759e73545458ece77d10fe2a7387b46de
zan <zan.kusterle@gmail.com>
2018-10-04 20:53:04 +0200

    docker-compose remove circle volumes

--- test/unit/component_snapshot_mixin.js
+++ test/unit/component_snapshot_mixin.js
@@ -20,7 +20,7 @@ export default function testSnapshots (component) {
 
         const usecases = getFlatUsecases(component)
 
-        jest.setTimeout(usecases.length * 20 * 1000) // 10 seconds per usecase
+        jest.setTimeout(usecases.length * 20 * 1000)
 
         for (let usecase of usecases) {
             describe(`usecase ${usecase.uniqueID}`, () => {
@@ -66,10 +66,10 @@ export default function testSnapshots (component) {
                     try {
                         expect(image).toMatchImageSnapshot({ customSnapshotIdentifier: `${component.name}-${usecase.uniqueID}` })
                     } catch (error) {
-                        const formattedUsecase = Object.keys(usecase)
+                        const formattedUsecase = Object.keys(usecase.data)
                             .sort((a, b) => a.localeCompare(b))
                             .map(key => {
-                                const value = typeof usecase[key] === 'object' ? JSON.stringify(usecase[key]).trim('"') : usecase[key]
+                                const value = typeof usecase.data[key] === 'object' ? JSON.stringify(usecase.data[key]).trim('"') : usecase.data[key]
                                 return `${key}: ${value}`
                             }).join('\n')
                         throw new Error(error.message + `\n${formattedUsecase}`)
