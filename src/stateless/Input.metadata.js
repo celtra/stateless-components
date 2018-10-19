b218e71c768c7d10c13680c09e7466229a5f87e8
zan <zan.kusterle@gmail.com>
2018-10-19 10:37:52 +0200

    Better input metadata, minor multiselect sorting fix

--- src/stateless/Input.metadata.js
+++ src/stateless/Input.metadata.js
@@ -9,5 +9,47 @@ export default {
         {
             label: 'Something',
         },
+        {
+            label: 'Something',
+            setup (vm) {
+                vm.focused = true
+                vm.overlay.open = true
+                return new Promise(resolve => setTimeout(resolve, 500))
+            },
+        },
+        {
+            label: 'Something',
+            setup (vm) {
+                vm.text = 'Input text'
+                return new Promise(resolve => setTimeout(resolve, 500))
+            },
+        },
+        {
+            type: 'password',
+            setup (vm) {
+                vm.text = 'Hidden text'
+            },
+        },
+        {
+            scopedSlots: {
+                before (h) {
+                    return h('span', 'px')
+                },
+            },
+        },
+        {
+            scopedSlots: {
+                left (h) {
+                    return h('span', 'px')
+                },
+            },
+        },
+        {
+            scopedSlots: {
+                right (h) {
+                    return h('span', 'px')
+                },
+            },
+        },
     ],
 }
